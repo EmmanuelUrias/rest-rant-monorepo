73425be651b20f8f6e2221325e30d66b285efa3e
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-12 21:26:25 -0700

    finished assignment

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const CurrentUser = createContext()
@@ -6,6 +6,20 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let res = await fetch('http://localhost:3007/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let user = await res.json()
+            console.log(user)
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -29,6 +29,7 @@ function LoginForm() {
 
         if(res.status === 200) {
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push('/')
         } else {
             setErrorMessage(data.message)
